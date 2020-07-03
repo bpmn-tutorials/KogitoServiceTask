11b3a927140f2c7fb9c8215e669d08b5dab078e9
Kirill Gaevskii <kgaevski@redhat.com>
2020-07-03 16:36:03 +0200

    Test OnEntry and OnExit fields

--- src/main/java/me/lazarotti/SomeClass.java
+++ src/main/java/me/lazarotti/SomeClass.java
@@ -8,15 +8,15 @@ import org.jboss.logging.Logger;
 public class SomeClass { 
  
     public void doSomething(Boolean boo) {
-        System.out.println("printing Boo " + boo);
+        System.out.println("Boo is " + boo);
     }
 
     public void doSomethingInt(Integer sInt) {
-        System.out.println("printing sInt + 2 = " + (sInt + 2));
+        System.out.println("printing " + sInt + " + 2 = " + (sInt + 2));
     }
 
     public void doSomethingFloat(Float sFloat) {
-        System.out.println("printing sFloat + 1.23 " + (sFloat + 1.23));
+        System.out.println("printing " + sFloat + " + 1.23 = " + (sFloat + 1.23));
     }
  
 }
